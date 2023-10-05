1b7cc5e46ebb00373ad60d24ae380f1da7b9aca4
Mindaugas Čėsna <m.cesn4@gmail.com>
2023-10-05 12:08:39 +0300

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,20 +15,20 @@ class MyApp extends StatelessWidget {
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primaryColor: Color(0xFF20C3AF),
+        primaryColor: const Color(0xFF20C3AF),
         scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme().apply(
-          bodyColor: Color(0xFF525464),
-          displayColor: Color(0xFF525464),
+        textTheme: const TextTheme().apply(
+          bodyColor: const Color(0xFF525464),
+          displayColor: const Color(0xFF525464),
         ),
-        inputDecorationTheme: InputDecorationTheme(
+        inputDecorationTheme: const InputDecorationTheme(
           border: InputBorder.none,
           filled: true,
           fillColor: Color(0xFFF7F7F7),
           contentPadding: EdgeInsets.symmetric(horizontal: 22),
         ),
         textSelectionTheme:
-            TextSelectionThemeData(cursorColor: Color(0xFF20C3AF)),
+            const TextSelectionThemeData(cursorColor: Color(0xFF20C3AF)),
         appBarTheme: const AppBarTheme(
           elevation: 0,
           backgroundColor: Colors.white,
